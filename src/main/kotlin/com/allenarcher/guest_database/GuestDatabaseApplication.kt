a93206a6520f082ccd391832fee5910c9b3e5189
allen <chunkystyles@gmail.com>
2026-05-24 12:15:03 -0400

    Initial commit. Project created with https://start.spring.io/

---
+++ src/main/kotlin/com/allenarcher/guest_database/GuestDatabaseApplication.kt
@@ -0,0 +1,11 @@
+package com.allenarcher.guest_database
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuestDatabaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<GuestDatabaseApplication>(*args)
+}
