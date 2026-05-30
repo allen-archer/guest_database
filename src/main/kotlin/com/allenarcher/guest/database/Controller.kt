c7e6ce72dd0d4d4374e09779be1c62f69f3e9e7a
allen <chunkystyles@gmail.com>
2026-05-30 10:39:44 -0400

    Adding @CrossOrigin to allow my HTML dashboard to work.

--- src/main/kotlin/com/allenarcher/guest/database/Controller.kt
+++ src/main/kotlin/com/allenarcher/guest/database/Controller.kt
@@ -9,6 +9,7 @@ import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.*
 import java.time.LocalDate
 
+@CrossOrigin
 @RestController
 class Controller(
     private val stayService: StayService,
